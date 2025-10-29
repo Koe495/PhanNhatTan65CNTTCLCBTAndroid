5d366b0609c4fa41b358854edbeba577651df986
Koe <tonphan495@gmail.com>
2025-10-29 07:51:49 +0700

    gk1.PhanNhatTan

--- gk1PhanNhatTan65133147/app/src/main/java/com/example/gk1phannhattan65133147/FoodList.java
+++ gk1PhanNhatTan65133147/app/src/main/java/com/example/gk1phannhattan65133147/FoodList.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class BMI_Calculator extends AppCompatActivity {
+public class FoodList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_bmi_calculator);
+        setContentView(R.layout.activity_food_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
